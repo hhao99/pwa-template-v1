03b70727f27e107ffad370f57eebcf7fd80761c9
Eric Hao <hhao99@gmail.com>
2023-05-26 13:23:11 -0500

    initial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="mx-auto">
+        <h1>Hello React PWA!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
